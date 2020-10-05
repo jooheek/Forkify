77fd176111a6cc981f031f639a0aea3ea34881e3
jooheek <kimjoohee713@gmail.com>
2020-10-05 23:30:19 +0900

    Add dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,30 @@
 const path = require('path');
 
 module.exports={
-    entry: './src/js/index.js',
+    entry: ['./src/js/index.js'],
     output:{
-        path:path.resolve(__dirname,'dist/js'),
-        filename: 'bundle.js'
+        path:path.resolve(__dirname,'dist'),
+        //path 는 절대경로여야한다.
+        filename: 'js/bundle.js'
     },
-    mode: 'development'
+    devServer: {
+        contentBase:'./dist'
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template :'./src/index.html'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    }
 };
