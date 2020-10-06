350ba9a76911d1c2418fd2823d70f358af0e3181
jooheek <kimjoohee713@gmail.com>
2020-10-06 23:04:03 +0900

    Add html-web-pack-plugin

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
 //Global app controller
 import num from './test';
-
-console.log(`i imported ${num} from another module`);
+const x = 23;
+console.log(`i imported ${num} from another module called test.js! Variable x is ${x}`);
