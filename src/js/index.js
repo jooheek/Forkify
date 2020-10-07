c0cd9928a81725c8a7588e6025428b1a63f8dbfc
jooheek <kimjoohee713@gmail.com>
2020-10-07 23:42:41 +0900

    Create search function temporarily

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,37 @@
-import str from './module/Search';
-//import {add as a,multiply as m,ID} from "./view/searchView";
-import * as searchView from "./view/searchView";
-console.log(`Using imported functions! ${searchView.add(ID,2)} and ${searchView.multiply(3,5)}.${str}.`);
+/*
+import Search from "./module/Search";
+
+/!** Global state of the app
+ * - search object
+ * - current recipe
+ * - shopping list object
+ * - liked recipes**!/
+
+const state={};
+
+const controlSearch =()=>{
+    //1)Get query from view
+    const query ='pizza';//TODO
+
+    if(query){
+        //2)new search object and add to state
+        state.search = new Search(query);
+
+        //3)Prepare UI for result
+
+        //4)Search for recipes
+        await state.search.getResults();
+
+        //5)Render results on UI
+        console.log(state.search.result);
+    }
+
+}
+
+document.querySelector('.search').addEventListener('submit',e =>{
+    e.preventDefault();
+    controlSearch();
+});
+const search = new Search('pizza');
+console.log(search);
+search.getResults();*/
